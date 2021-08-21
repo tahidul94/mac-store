43422ff285cbe51d3a4bb2875c727ce81c3232bb
tahidul94 <tahidul94@gmail.com>
2021-08-21 20:40:24 +0600

    all code revised

--- js/app.js
+++ js/app.js
@@ -4,49 +4,55 @@ function setTotalPrice() {
     let a = parseInt(document.getElementById('ram-cost').innerText);
     let b = parseInt(document.getElementById('ssd-cost').innerText);
     let c = parseInt(document.getElementById('delivery-cost').innerText);
-    let sum = d + a + b + c;
+    let sum = d + a + b + c;  //total sum before pomo code
     document.getElementById('total-price').innerText = sum;
     document.getElementById('grand-total').innerText = sum;
 }
+function getEvent(productId, price) {
+    document.getElementById(productId).innerText = price;
+}
+//sum after pomo code
 function getDiscountPrice() {
     let total = document.getElementById('total-price').innerText;
     const pomoCheck = document.getElementById('pomo-code').value;
-    if (pomoCheck === 'stevekaku') {
+    pomoLower = pomoCheck.toLowerCase(); // all input will be lower Case
+    if (pomoLower === 'stevekaku') {
         const discount = total * 20 / 100;
         total -= discount;
-        document.getElementById('grand-total').innerText = total;
+        document.getElementById('grand-total').innerText = total; // total after pomo
     }
 }
+//Event handeler 
 document.getElementById('ram-price-8gb').addEventListener('click', function () {
-    document.getElementById('ram-cost').innerText = 0;
+    getEvent('ram-cost', 0);
     setTotalPrice();
 })
 document.getElementById('ram-price-16gb').addEventListener('click', function () {
-    document.getElementById('ram-cost').innerText = 180;
+    getEvent('ram-cost', 180);
     setTotalPrice();
 })
 document.getElementById('ssd-price-256gb').addEventListener('click', function () {
-    document.getElementById('ssd-cost').innerText = 0;
+    getEvent('ssd-cost', 0);
     setTotalPrice();
 })
 document.getElementById('ssd-price-512gb').addEventListener('click', function () {
-    document.getElementById('ssd-cost').innerText = 100;
+    getEvent('ssd-cost', 100);
     setTotalPrice();
 })
 document.getElementById('ssd-price-1024gb').addEventListener('click', function () {
-    document.getElementById('ssd-cost').innerText = 180;
+    getEvent('ssd-cost', 180);
     setTotalPrice();
 })
 document.getElementById('delivery-15days').addEventListener('click', function () {
-    document.getElementById('delivery-cost').innerText = 0;
+    getEvent('delivery-cost', 0);
     setTotalPrice();
 })
 document.getElementById('delivery-3days').addEventListener('click', function () {
-    document.getElementById('delivery-cost').innerText = 20;
+    getEvent('delivery-cost', 20);
     setTotalPrice();
 })
 document.getElementById('pomo-apply').addEventListener('click', function () {
-    getDiscountPrice()
+    getDiscountPrice();
 })
 
 
